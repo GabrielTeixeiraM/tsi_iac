951f747b8dc7d97cc8145768e8833e7dbb0fe8cd
Gabriel <gabrielteixeiramori@gmail.com>
2025-11-13 20:55:11 -0300

    a

--- calc.php
+++ calc.php
@@ -1,7 +1,6 @@
 <?php
-// calc.php — exemplo simples de operação PHP
 $a = 5;
 $b = 3;
 $soma = $a + $b;
-echo "A  soma de $a + $b é igual a $soma\n";
+echo "A soma de $a + $b é igual a $soma\n";
 ?>
